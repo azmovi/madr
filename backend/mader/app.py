4a6fcc1922b3e56e7313932781885776e867adc5
azevedo <azevedoantoniocicero@gmail.com>
2024-06-23 20:48:28 -0300

    agr da pra consumir a api

--- backend/mader/app.py
+++ backend/mader/app.py
@@ -1,12 +1,22 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from mader.routes import conta
 from mader.schemas import Message
 
 app = FastAPI()
 app.include_router(conta.router)
+
+origins = ["http://localhost:5173"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"]
+)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const message = ref('');
+
+async function teste() {
+    try {
+        const response = await fetch('http://localhost:8000/');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        message.value = data.message;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <h1>Olá mundo</h1>
+        <button @click="teste">Teste de API</button>
+        <p>{{ message }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+/* Estilos locais para o componente */
+.wrapper {
+    text-align: center;
+    margin-top: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+    margin-top: 10px;
 }
 </style>
+
