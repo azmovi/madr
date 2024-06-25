11dbcb29c688e7ccecc65499e33f6824e588a635
azevedo <azevedoantoniocicero@gmail.com>
2024-06-24 21:44:32 -0300

    desgraça online

--- backend/mader/models.py
+++ backend/mader/models.py
@@ -5,7 +5,7 @@ table_registry = registry()
 
 @table_registry.mapped_as_dataclass
 class User:
-    __tablename__ = 'Users'
+    __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
