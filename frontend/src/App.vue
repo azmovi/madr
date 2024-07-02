ffb676943b8a3c0eee38604343394322b824444c
azevedo <azevedoantoniocicero@gmail.com>
2024-07-01 23:13:09 -0300

    desgraça dificil to perdido

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,16 +4,35 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <div class="wrapper">
-        <h1>Mader</h1>
-    </div>
+      <h1>MADR: Meu Acervo Digital de Romancistas</h1>
+      <div class="user-session">
+          <button @click=''></button> 
+      </div>
   </header>
 
   <main>
-      <h3>Sou um componente</h3>
-     <HelloWorld />
+     <!-- <HelloWorld /> -->
+     ola
   </main>
 </template>
 
 <style scoped>
+header{
+    position: fixed;
+    top: 0;
+    margin: 3%;
+
+    display: grid;
+    grid-auto-flow: column;
+    
+}
+.user-session{
+    border: 2px solid;
+    margin: 5%;
+}
+
+main{
+    border: 2px solid;
+    margin-top: 15%;
+}
 </style>
