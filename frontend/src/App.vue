b9bca123b536c68737e5c24b999dd0d54747bba5
azevedo <azevedoantoniocicero@gmail.com>
2024-07-02 13:02:52 -0300

    desgraça

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,11 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-      <h1>MADR: Meu Acervo Digital de Romancistas</h1>
-      <div class="user-session">
-          <button @click=''></button> 
-      </div>
-  </header>
-
-  <main>
-     <!-- <HelloWorld /> -->
-     ola
-  </main>
+    <div class="flex flex-col min-h-screen font-FiraCode bg-black-primary">
+        <RouterView />
+        <SiteNav />
+    </div>
 </template>
 
-<style scoped>
-header{
-    position: fixed;
-    top: 0;
-    margin: 3%;
-
-    display: grid;
-    grid-auto-flow: column;
-    
-}
-.user-session{
-    border: 2px solid;
-    margin: 5%;
-}
-
-main{
-    border: 2px solid;
-    margin-top: 15%;
-}
-</style>
+<script>
+import { RouterView } from "vue-router";
+import SiteNav from "./components/SiteNav.vue";
+</script>
---
+++ frontend/src/components/SiteNav.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="sticky top-0 bg-red-secundary shadow-lg">
+        <nav class="conteiner flex flex-col
+            sm:flex-row items-center gap-4 text-gray-50 py-6"
+        >
+        <div class="flex items-center gap-3 flex-1">
+            <button type="button"> login / cadastro</button>
+        </div>
+        </nav>
+        <p>adfas</p>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
