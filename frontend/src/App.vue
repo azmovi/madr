2afef6eb8288588bdbaa187e23691da410fcf779
azevedo <azevedoantoniocicero@gmail.com>
2024-07-02 20:12:43 -0300

    Na moral vai se fuder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import SiteNav from '@/components/SiteNav.vue'
+</script>
+
 <template>
-    <div class="flex flex-col min-h-screen font-FiraCode bg-black-primary">
-        <RouterView />
-        <SiteNav />
-    </div>
+    <SiteNav />
+    <RouterView />
+    ola
 </template>
 
-<script>
-import { RouterView } from "vue-router";
-import SiteNav from "./components/SiteNav.vue";
-</script>
+<style scoped>
+</style>
--- frontend/src/components/SiteNav.vue
+++ frontend/src/components/SiteNav.vue
@@ -1,16 +1,41 @@
-<template>
-    <header class="sticky top-0 bg-red-secundary shadow-lg">
-        <nav class="conteiner flex flex-col
-            sm:flex-row items-center gap-4 text-gray-50 py-6"
-        >
-        <div class="flex items-center gap-3 flex-1">
-            <button type="button"> login / cadastro</button>
-        </div>
-        </nav>
-        <p>adfas</p>
-    </header>
-</template>
-
 <script setup>
 import { RouterLink } from "vue-router";
 </script>
+
+<template>
+    <header>
+        <nav>
+            <RouterLink :to="{ name: 'home' }">
+                <div class="home">
+                    <h1>MADR</h1>
+                </div>
+            </RouterLink>
+            <ul>
+                <li class="item"> Livros </li>
+                <li class="item"> Romancistas </li>
+            </ul>
+            <button type="button"> Sign in / Sign up </button>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+nav{
+    padding: 5%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+}
+
+ul{
+    display: flex;
+    list-style: none;
+    gap: 12%;
+}
+
+
+.home{
+  color: #d3d3d3;
+}
+</style>
