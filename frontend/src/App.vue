42e9af30f2f6f74537e5df6959158327c5460f63
azevedo <azevedoantoniocicero@gmail.com>
2024-07-01 21:01:41 -0300

    refazendo o sistema do frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,19 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const message = ref('');
-
-async function teste() {
-    try {
-        const response = await fetch('http://localhost:8000/');
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        const data = await response.json();
-        message.value = data.message;
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
-}
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <header>
     <div class="wrapper">
-        <h1>Olá mundo</h1>
-        <button @click="teste">Teste de API</button>
-        <p>{{ message }}</p>
+        <h1>Mader</h1>
     </div>
+  </header>
+
+  <main>
+      <h3>Sou um componente</h3>
+     <HelloWorld />
+  </main>
 </template>
 
 <style scoped>
-/* Estilos locais para o componente */
-.wrapper {
-    text-align: center;
-    margin-top: 20px;
-}
-
-button {
-    margin-top: 10px;
-}
 </style>
-
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('')
+
+async function teste() {
+    await fetch('http://localhost:8000/')
+        .then((res) => res.json())
+        .then((json) => message.value = json.message)
+        .catch((err) => message.value = err)
+};
+</script>
+
+<template>
+  <div class="basicos">
+        <button @click="teste"> Teste </button>
+        <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
