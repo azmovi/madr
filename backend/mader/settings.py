66875a120f3649f18475b0a84e6be024eb13dc91
Antonio Cicero Azevedo <azevedoantoniocicero@gmail.com>
2025-05-24 13:14:41 -0300

    nada funciona

--- backend/mader/settings.py
+++ backend/mader/settings.py
@@ -5,10 +5,10 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env', env_file_encoding='utf-8', extra='ignore'
     )
-    DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    DATABASE_URL: str = 'default'
+    SECRET_KEY: str = 'default'
+    ALGORITHM: str = 'default'
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 0
 
 
 settings = Settings()
